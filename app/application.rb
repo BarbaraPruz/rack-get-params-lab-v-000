6ab3ceddab669bbf0d964ea0fab0e0ec085ea66d
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-05-07 23:51:13 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,8 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
-
+  @@cart = []
+  
   def call(env)
     resp = Rack::Response.new
     req = Rack::Request.new(env)
@@ -10,9 +11,25 @@ class Application
       @@items.each do |item|
         resp.write "#{item}\n"
       end
+      
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+      
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      if  @@items.include?(search_term)
+        cart << item
+        resp.write("Added #{item}")
+      else
+        resp.write("Couldn't find #{item}")
+      end
+      
+    elsif req.path.match(/cart/)
+      resp.write "Your cart is empty" if @@cart.count==0
+      @@cart.each do |item|
+        resp.write "#{item}\n"
+        
     else
       resp.write "Path Not Found"
     end
